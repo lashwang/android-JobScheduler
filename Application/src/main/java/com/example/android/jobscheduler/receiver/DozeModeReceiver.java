bddb5076d7e9b70f2a07bc9de62da605eba2e451
simon <lashwang@outlook.com>
2018-10-15 15:57:36 +0800

    add alarm test case.

---
+++ Application/src/main/java/com/example/android/jobscheduler/receiver/DozeModeReceiver.java
@@ -0,0 +1,12 @@
+package com.example.android.jobscheduler.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class DozeModeReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
